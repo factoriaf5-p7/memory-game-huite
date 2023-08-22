3831a03a6ef704ae9a0efe06790f35c118ce2d11
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-08-22 22:25:55 +0200

    #4 call data.json

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,28 +1,31 @@
-import Card from '../Card/Card'; 
+import Card from '../Card/Card';
 import './Board.css';
+import data from './../../data/data.json';
 
-function Grid() {
-  const numRows = 6;
+/* aca creamos un tablero de cartas utilizando datos aleatorios del archivo data.json. y se usa el componente Card para renderizar cada carta en la cuadrícula. */
+
+function Board() {
+  const numRows = 6; 
   const numCols = 4;
 
-  // Crea una matriz bidimensional para representar la cuadrícula
-  const grid = [];
+  const shuffledData = [...data].sort(() => Math.random() - 0.5); //// Se barajan los datos de las cartas aleatoriamente
 
+  const grid = [];
+  // Hacemos una cuadrícula de cartas con un bucle anidado para repetir las cards una y otra vez
   for (let row = 0; row < numRows; row++) {
     const rowItems = [];
     for (let col = 0; col < numCols; col++) {
-      // Renderiza el componente Card en cada celda
-      rowItems.push(<Card key={`${row}-${col}`} />);
+      const cardData = shuffledData[row * numCols + col];
+      rowItems.push(<Card key={`${row}-${col}`} img={cardData.img} />);
     }
-    // Agrega la fila completa a la cuadrícula
     grid.push(<div key={row}>{rowItems}</div>);
   }
 
   return (
-    <div id="memory_board">
-      {grid}
+    <div className="board-container"> {/*Se crea el tablero*/}
+      <div id="memory_board">{grid}</div> {/*Se crea el tablero con las cartas*/}
     </div>
   );
 }
 
-export default Grid;
+export default Board;
--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './Card.css';
 
+/* definimos el componente Card que representa una carta individual. Al hacer clic en la carta, cambia su estado para mostrar la imagen del detras.El componente recibe la URL de la imagen como prop img. */
+
 interface CardProps {
-  // Si tienes propiedades, defínelas aquí
+  img: string;
 }
 
-function Card(props: CardProps) {
+function Card({ img }: CardProps) {
   const [isFlipped, setIsFlipped] = useState<boolean>(false);
 
   const handleClick = () => {
@@ -15,8 +17,8 @@ function Card(props: CardProps) {
   return (
     <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
       <div className="card-inner">
-        <div className="card-front">F</div>
-        <div className="card-back">B</div>
+        <div className="card-front"></div>
+        <div className="card-back" style={{ backgroundImage: `url('/img/${img}')` }}></div>
       </div>
     </div>
   );
