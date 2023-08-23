f8f04ef294e7049f98d0a29bd14ca3ffefd68cbd
huilenpe@gmail.com <131238839+HuilenPe@users.noreply.github.com>
2023-08-24 00:34:52 +0200

    update back

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { AppController } from './app.controller';
-import { Image, ImageSchema } from './image.schema';
-import { ImageModule } from './image/image.module';
-import { ImageService } from './image/image.service';
+import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://huilenpe:fni71E1yon1TDWg0@cluster0.kjy0y3t.mongodb.net/Memory-game',
     ),
-    MongooseModule.forFeature([{ name: Image.name, schema: ImageSchema }]),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'frontend/public/img'), // Apunta a la carpeta "public"
-    }),
-    ImageModule,
+    GameModule,
   ],
-  controllers: [AppController],
-  providers: [{ ImageService }],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: '*', // Permitir todas las solicitudes de cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  };
+  app.enableCors(corsOptions);
+
+  const validationPipeOptions: ValidationPipeOptions = {
+    transform: true, // Transformar el cuerpo de la solicitud a objetos
+    whitelist: true, // Solo permitir propiedades decoradas en las clases DTO
+    forbidNonWhitelisted: true, // Rechazar solicitudes con propiedades no permitidas
+  };
+  app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
   await app.listen(3000);
   console.log('La aplicación se ha iniciado correctamente.');
 }
